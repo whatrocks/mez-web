789a0fe50ac20cf24fb1d2ca1484c27c1d078466
Charlie Harrington <charlie@whatrocks.org>
2018-02-13 22:04:32 -0800

    clean up login form a bit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -23,30 +23,39 @@ export default class LoginForm extends Component {
     const errors = this.props.errors || {};
     return (
       <div>
-        <form onSubmit={this.onSubmit}>
-          <h1>Authentication</h1>
-          {
-            errors.non_field_errors ?
-            <div>{errors.non_field_errors}</div>
-            : ""
-          }
-          <TextInput
-            name="username"
-            label="Username"
-            error={errors.username}
-            onChange={this.handleInputChange}
-          />
-          <TextInput
-            name="password"
-            label="Password"
-            error={errors.password}
-            type="password"
-            onChange={this.handleInputChange}
-          />
-          <button type="submit">
-          Log In
-          </button>
-        </form>
+         <section className="hero is-large">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <form onSubmit={this.onSubmit}>
+                  <h1>Log In</h1>
+                  {
+                    errors.non_field_errors ?
+                    <div>{errors.non_field_errors}</div>
+                    : ""
+                  }
+                  <TextInput
+                    name="username"
+                    label="Username"
+                    error={errors.username}
+                    onChange={this.handleInputChange}
+                  />
+                  <TextInput
+                    name="password"
+                    label="Password"
+                    error={errors.password}
+                    type="password"
+                    onChange={this.handleInputChange}
+                  />
+                  <button type="submit">
+                  Log In
+                  </button>
+                </form>
+                <p>By continuing, you agree to Mezcal's Terms of Service</p>
+                <p>Forgot password?</p>
+                <p>Don't have an account? Sign up.</p>
+              </div>
+            </div>
+          </section>
       </div>
     )
   }
