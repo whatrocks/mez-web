9cf229376747b04adb2553ef30a6f7864da4725b
Charlie Harrington <charlie@whatrocks.org>
2018-03-26 22:40:33 -0700

    individual event ui

--- src/components/MezEvent.js
+++ src/components/MezEvent.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import _ from "lodash";
 
+import { invertedRules, repeatColors } from "../utils/events";
+import { formatDateTime } from "../utils/time";
+
+import style from "./MezEvent.scss";
+
 class MezEvent extends Component {
 
   componentDidMount() {
@@ -13,11 +18,27 @@ class MezEvent extends Component {
 
   render() {
     const { event } = this.props;
+    const occurrences = _.get(event, 'occurrences', []);
+    const nextEvent = occurrences[0];
+    const pattern = _.get(nextEvent, 'repeat', '');
+    const repeat = invertedRules[pattern];
+    const color = repeatColors[repeat];
     return (
       <div className="container">
-        <h3>Event</h3>
-        <hr />
-        <h2>{event.title}</h2>
+        <div className={`box ${style.event}`}>
+          <h2 className="title">{event.title}</h2>
+          <span className={`tag is-primary ${style.repeatTag} ${color}`}>{repeat}</span>
+          <ul>
+            <li className={style.eventItem}>
+              <div><strong>START</strong></div>
+              <div>{nextEvent && nextEvent.start && formatDateTime({ datetime: nextEvent.start })}</div>
+            </li>
+            <li className={style.eventItem}>
+              <div><strong>END</strong></div>
+              <div>{nextEvent && nextEvent.end && formatDateTime({ datetime: nextEvent.end })}</div>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/utils/events.js
+++ src/utils/events.js
@@ -1,7 +1,19 @@
+import _ from "lodash";
+
 export const RRULES = {
   'One-time': '',
   Daily: 'RRULE:FREQ=DAILY',
   Weekly: 'RRULE:FREQ=WEEKLY',
   Monthly: 'RRULE:FREQ=MONTHLY',
   Yearly: 'RRULE:FREQ=YEARLY'
+}
+
+export const invertedRules = _.invert(RRULES);
+
+export const repeatColors = {
+  'One-time': 'is-primary',
+  Daily: 'is-info',
+  Weekly: 'is-success',
+  Monthly: 'is-warning',
+  Yearly: 'is-danger'
 }
